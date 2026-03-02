714d151a7a23cd8be9e1fb4d11df3f59d0771fa7
makise <1157552798@qq.com>
2026-03-02 10:00:00 +0800

    feat: 项目初始化，搭建 Spring Boot 后端与 Vue 前端基础框架

---
+++ src/main/java/com/yupi/zerocodeplatform/ZerocodePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.zerocodeplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZerocodePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZerocodePlatformApplication.class, args);
+    }
+
+}
