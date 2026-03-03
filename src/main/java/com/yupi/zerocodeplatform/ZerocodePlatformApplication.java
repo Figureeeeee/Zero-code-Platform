006a522ea480ceceec75c4b7de8b5e797ef0dc3d
makise <1157552798@qq.com>
2026-03-03 10:00:00 +0800

    feat: 用户模块实现

--- src/main/java/com/yupi/zerocodeplatform/ZerocodePlatformApplication.java
+++ src/main/java/com/yupi/zerocodeplatform/ZerocodePlatformApplication.java
@@ -1,9 +1,11 @@
 package com.yupi.zerocodeplatform;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yupi.zerocodeplatform.mapper")
 public class ZerocodePlatformApplication {
 
     public static void main(String[] args) {
