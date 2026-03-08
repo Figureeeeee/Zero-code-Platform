d5235ea37b42bcc6891fce32008d32cb179673e8
makise <1157552798@qq.com>
2026-03-08 10:00:00 +0800

    feat: 功能扩展（截图功能、项目打包下载、AI智能路由）

--- src/main/java/com/yupi/zerocodeplatform/service/AppService.java
+++ src/main/java/com/yupi/zerocodeplatform/service/AppService.java
@@ -2,6 +2,7 @@ package com.yupi.zerocodeplatform.service;
 
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
+import com.yupi.zerocodeplatform.model.dto.app.AppAddRequest;
 import com.yupi.zerocodeplatform.model.dto.app.AppQueryRequest;
 import com.yupi.zerocodeplatform.model.entity.App;
 import com.yupi.zerocodeplatform.model.entity.User;
@@ -20,21 +21,38 @@ public interface AppService extends IService<App> {
     /**
      * 通过对话生成应用代码
      *
-     * @param appId 应用 ID
-     * @param message 提示词
+     * @param appId     应用 ID
+     * @param message   提示词
      * @param loginUser 登录用户
      * @return
      */
     Flux<String> chatToGenCode(Long appId, String message, User loginUser);
 
     /**
+     * 创建应用
+     *
+     * @param appAddRequest
+     * @param loginUser
+     * @return
+     */
+    Long createApp(AppAddRequest appAddRequest, User loginUser);
+
+    /**
      * 应用部署
      *
-     * @param appId 应用 ID
+     * @param appId     应用 ID
      * @param loginUser 登录用户
      * @return 可访问的部署地址
      */
     String deployApp(Long appId, User loginUser);
+
+    /**
+     * 异步生成应用截图并更新封面
+     *
+     * @param appId  应用ID
+     * @param appUrl 应用访问URL
+     */
+    void generateAppScreenshotAsync(Long appId, String appUrl);
 
     /**
      * 获取应用封装类
